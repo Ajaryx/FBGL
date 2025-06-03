38106f4307499336578f777c0f0622a5a100bbf9
Ajarys <impressum15@gmail.com>
2025-06-03 14:18:51 +0200

    fixed more CMake bugs for Windows

--- cmake/CompileGLEWLib.cmake
+++ cmake/CompileGLEWLib.cmake
@@ -6,6 +6,7 @@ if(FBGL_STATIC)
 add_library(${GLEW_LIB} STATIC ${GLEW_SOURCES})
 
 message(STATUS ${GLEW_SOURCE_DIR})
+target_compile_definitions(${GLEW_LIB} PUBLIC GLEW_STATIC)
 message(STATUS "GLEW LIB STATIC BUILD")
 endif()
 
@@ -13,8 +14,9 @@ endif()
 if(FBGL_SHARED)
 add_library(${GLEW_LIB} SHARED ${GLEW_SOURCES})
 
+target_compile_definitions(${GLEW_LIB} PUBLIC GLEW_BUILD)
 message(STATUS "GLEW LIB SHARED BUILD")
 endif()
 
-
+target_link_libraries(${GLEW_LIB} PUBLIC OpenGL::GL)
 endfunction(compile_glew_lib)
